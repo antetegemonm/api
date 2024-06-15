bba0799a482406e55e45fe220101c72538cf3b09
antetegemomn <alexlp2k6@gmail.com>
2024-06-15 00:00:10 -0300

    feat:create api

---
+++ server.js
@@ -0,0 +1,82 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+const prisma = new PrismaClient()
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.post('/usuarios' ,async (req, res)=>
+{
+await prisma.user.create({
+     data:{
+          email: req.body.email,
+          name: req.body.name,
+          age:req.body.age
+     }
+})
+
+res.status(201).json(req.body)
+
+})
+
+
+app.get('/usuarios', async (req , res) =>{
+
+    const users =  await prisma.user.findMany()
+
+     res.status(200).json(users)
+})
+
+
+
+
+app.put('/usuarios/:id' ,async (req, res)=>
+     {
+        
+
+    await prisma.user.update({
+         
+     where:{
+           id: req.params.id
+          },
+
+          data:{
+               email: req.body.email,
+               name: req.body.name,
+               age:req.body.age
+          }
+     })
+     
+     res.status(201).json(req.body)
+     
+     })
+
+     app.delete('/usuarios/:id',async (req, res)=>
+     {
+
+          await prisma.user.delete({
+            where:{
+               id: req.params.id
+            }
+            
+          })
+
+          res.status(200).json({message:"usuário deletado com sucesso"})
+     })
+
+     app.listen(3000)
+
+/*
+2- npx prisma studio para utilizar o prisma
+3- baixar thunder.client
+4- verificar se tem que baixar o prisma (creio que não mas qualquer coisa é só baixar o prisma)
+
+node --watch server.js -> reestarta o servidor automaticamente toda vez 
+o projeto for salvo */
+
+//alex 
+//leco123
